22c25bbc67057df53e59e0064b96024e301c59b4
LusoNetto <lusonetto137@outlook.com>
2025-03-16 11:25:18 -0300

    ✨ feat: complete backend and database setup
    - Updated `backend/controllers/auth.controller.js` to refine authentication logic
    - Created `backend/lib/db.js` to handle database connection and configuration
    - Added `backend/models/user.model.js` to define the user schema for MongoDB
    - Modified `backend/routes/auth.routes.js` to include refined authentication routes
    - Updated `backend/server.js` to integrate the database connection and route setup
    
    This commit completes the backend and database setup, providing a functional foundation for user authentication and data management in the e-commerce application.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,24 @@
-export const signup = (req, res) => {
-  res.send("Sign up route called");
-}
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, password, name });
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    console.log("Error: ", error.message);
+    res.status(500).json({message: error.message});
+  }
+};
 
 export const login = (req, res) => {
   res.send("login up route called");
-}
+};
 
 export const logout = (req, res) => {
   res.send("logout up route called");
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
